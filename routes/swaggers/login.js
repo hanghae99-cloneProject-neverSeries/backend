dc422933b998434ccfd3d9418b6c8aad3d93704d
Jjeong-Ho <84619866+jeangho293@users.noreply.github.com>
2021-10-19 02:27:26 +0900

    Create swagger API login

---
+++ routes/swaggers/login.js
@@ -0,0 +1,27 @@
+/**
+ * @swagger
+ * /login:
+ *  post:
+ *    summary: 로그인 API
+ *    description: JWT 토큰 및 사용자 인증을 위한 API 입니다.
+ *    tags:
+ *      - 로그인
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              userId:
+ *                type: string
+ *                required: true
+ *              pw:
+ *                type: string
+ *                required: true
+ *            example:
+ *              userId: test
+ *              pw: "1234"
+ *    responses:
+ *      200:
+ *        description: OK
+ */
