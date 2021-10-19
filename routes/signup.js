4787ac4fd9491327ab5b01ef40d38d1dd5e07f68
Jjeong-Ho <84619866+jeangho293@users.noreply.github.com>
2021-10-19 14:51:47 +0900

    김정호: joi 검증 추가

--- routes/signup.js
+++ routes/signup.js
@@ -2,20 +2,31 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/users');
+const joi = require('joi');
 
 router.route('')
   // 회원 가입 등록
-  .post(async (req, res) => {
+  .post(async (req, res, next) => {
     try {
-      const {userId, pw, pwCheck, nickname} = req.body;
+      const IdFilter = /^[a-z0-9_-]{5,20}$/;  // 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.
+      const pwFilter = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,16}$/; //8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
+
+      const userIdSchema = joi.object({
+        userId: joi.string().min(5).max(20).pattern(new RegExp(IdFilter)).required(),
+        pw: joi.string().min(8).max(16).pattern(new RegExp(pwFilter)).required(),
+        pwCheck: joi.ref('pw'),
+        nickname: joi.string().min(1).required(),
+      });
+
+      const {userId, pw, pwCheck, nickname} = await userIdSchema.validateAsync(req.body);
 
       // 존재하는 아이디 중복 체크
       if (await User.findOne({where: {userId}})) {
         res.send({msg: '이미 존재하는 아이디입니다.'});
       } else {
         // userID, pw 정규식 표현
-        const IdFilter = /^[a-z0-9_-]{5,20}$/;  // 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.
-        const pwFilter = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,16}$/; //8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
+        // const IdFilter = /^[a-z0-9_-]{5,20}$/;  // 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.
+        // const pwFilter = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,16}$/; //8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
 
         if (!IdFilter.test(userId)) {
           res.send({msg: '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.'});
@@ -33,7 +44,8 @@ router.route('')
         }
       }
     } catch (err) {
-      res.send(`${req.url}, ${req.method}, ${req.error}`);
+      console.log(`${req.url}, ${req.method}, ${req.error}`);
+      next(err)
     }
   });
 
