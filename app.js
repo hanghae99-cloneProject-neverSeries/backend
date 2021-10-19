8199dc07dfd974f3cc925a8938c4ef6cc8716db3
Jjeong-Ho <84619866+jeangho293@users.noreply.github.com>
2021-10-20 00:45:34 +0900

    김정호: 미들웨어 위치 변경

--- app.js
+++ app.js
@@ -30,16 +30,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
 app.use("/test", testRouter);
-
 app.use("/signup", signupPage);
 app.use("/login", loginPage);
+app.use("/novel", novelRouter);
+app.use("/reviews", reviewsRouter);
 
 // 에러 핸들러 미들웨어 --> 김정호: app.js 추가된 부분
 app.use(errorHandler.routerError);
 app.use(errorHandler.errorHandler);
-
-app.use("/novel", novelRouter);
-app.use("/reviews", reviewsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}/api`);
