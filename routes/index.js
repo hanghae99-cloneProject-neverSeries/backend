50ac9bd525eb45575959444d9829b4793a2b049e
shinsw627 <shinsw627@naver.com>
2021-10-20 22:10:33 +0900

    신성웅 : 테스트코드 연습

--- routes/index.js
+++ routes/index.js
@@ -1,112 +1,14 @@
-<<<<<<< HEAD
 const express = require("express");
-const Product = require("../models/products");
 const router = express.Router();
-const { User } = require("../models");
-const BuyProduct = require("../models/buyproducts");
 const authUser = require("../middlewares/auth-middleware");
-const { Sequelize, Op } = require("sequelize");
 
 //홈 화면 조회
-router.get("/", async (req, res, next) => {
-  try {
-    const products = await Product.findAll({});
-    res.send({ result: products });
-  } catch (err) {
-    res.status(400).send({
-      //에러처리
-    });
-  }
-});
-
+router.get("/", home);
 //마이페이지
-router.get("/mypage", authUser, async (req, res, next) => {
-  try {
-    const { user_id } = res.locals;
-    const buyproduct = await BuyProduct.findAll({
-      where: { userId: user_id },
-      raw: true,
-      attributes: [
-        [Sequelize.fn("DISTINCT", Sequelize.col("productId")), "productId"],
-      ],
-    });
-
-    const id = buyproduct.map((value, index) => {
-      return { id: value.productId };
-    });
-
-    const products = await Product.findAll({
-      where: {
-        [Op.or]: id,
-      },
-      raw: true,
-    });
-    console.log(products);
-    res.send(products);
-  } catch (err) {
-    next(err);
-  }
-});
-
+router.get("/mypage", authUser, mypage);
 //머핀 충전
-router.post("/muffin", authUser, async (req, res, next) => {
-  try {
-    const { userId } = res.locals;
-    const muffin = req.body.muffin + res.locals.muffin;
-    await User.update({ muffin }, { where: { userId } });
-    res.send({ msg: "머핀이 충전되었습니다" });
-  } catch (err) {
-    next(err);
-  }
-});
-
+router.post("/muffin", authUser, buyMuffin);
 //상품 구매
-router.put("/buy", authUser, async (req, res, next) => {
-  try {
-    const { userId, user_id } = res.locals;
-    const { productId, round } = req.body;
-    const userInfo = await User.findOne({
-      raw: true, // 모델 형식이 아닌 데이터 형식으로 변환
-      where: { userId },
-      attributes: ["muffin"],
-    });
-    const userMuffin = userInfo.muffin;
-    if (userMuffin <= 2) {
-      res.send({ msg: "쿠키가 부족합니다." });
-    } else {
-      if (
-        (
-          await BuyProduct.findAll({
-            where: { userId: user_id, productId, round },
-          })
-        ).length > 0
-      ) {
-        res.send({ msg: "이미 구매한 도서입니다." }); // 구매안하기전 : 구매   구매한 후: 보기 --> Todo 프론트 작업 후 불필요한 부분
-      } else {
-        await User.update({ muffin: userMuffin - 3 }, { where: { userId } });
-        await BuyProduct.create({ userId: user_id, productId, round });
-        res.send({ msg: "구매 완료" });
-      }
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-=======
-const express = require('express');
-const router = express.Router();
-const authUser = require('../middlewares/auth-middleware');
-const {home, mypage, buyMuffin, buyProduct} = require('./controller/index');
+router.put("/buy", authUser, buyProduct);
 
-//홈 화면 조회
-router.get('/', home);
-//마이페이지
-router.get('/mypage', authUser, mypage);
-//머핀 충전
-router.post('/muffin', authUser, buyMuffin);
-//상품 구매
-router.put('/buy', authUser, buyProduct);
-
-
->>>>>>> f847bc638bb87af27411253ef248c727d50ae556
 module.exports = router;
--- routes/novel.js
+++ routes/novel.js
@@ -1,15 +1,6 @@
 const express = require("express");
-<<<<<<< HEAD
 const { getProduct, createProduct, handleLike } = require("./controller/novel");
-const authMiddleware = require("../middlewares/auth-middleware");
-=======
-const {
-  getProduct,
-  createProduct,
-  handleLike,
-} = require('./controller/novel')
-const authUser = require('../middlewares/auth-middleware')
->>>>>>> f847bc638bb87af27411253ef248c727d50ae556
+const authUser = require("../middlewares/auth-middleware");
 
 const router = express.Router();
 
@@ -18,10 +9,6 @@ router.get("/:productId", getProduct);
 //product 추가(임시 테스트 데이터 추가용)
 router.post("/", createProduct);
 //좋아요 추가, 삭제
-<<<<<<< HEAD
-router.post("/like", handleLike);
-=======
-router.post('/like', authUser, handleLike)
->>>>>>> f847bc638bb87af27411253ef248c727d50ae556
+router.post("/like", authUser, handleLike);
 
 module.exports = router;
