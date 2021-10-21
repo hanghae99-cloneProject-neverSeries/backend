e37598b2b152562fd2c8ba5ef1bbacae06f659f7
jeangho <jeangho293@naver.com>
2021-10-21 18:45:23 +0900

    김정호: 회원 가입 단방향 코드 분리

--- routes/signup.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup } = require('./controller/signup')
-
-router.route('')
-  // 회원 가입 등록
-  .post(signup);
-
-module.exports = router;
