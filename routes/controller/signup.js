c8606d8d454701697c6ef5c493696ec207865893
jeangho <jeangho293@naver.com>
2021-10-21 18:45:19 +0900

    김정호: 회원 가입 단방향 코드 분리

--- routes/controller/signup.js
+++ routes/controller/signup.js
@@ -1,6 +1,4 @@
-const bcrypt = require('bcrypt');
-const User = require('../../models/users');
-const {signJoi, dupCheckId} = require('./unit/signup-joi');
+const {signJoi, dupCheckId, createUser} = require('./unit/signup-joi');
 const printError = require('./unit/error');
 
 const signup = async (req, res) => {
@@ -8,12 +6,8 @@ const signup = async (req, res) => {
     const {userId, pw, nickname} = await signJoi(req);
     // 아이디 중복 및 닉네임 중복 검사
     await dupCheckId(userId, nickname);
-
-    // 모든 조건 통과 시 비밀번화 단방향 암호화 및 user 생성
-    const EncryptPw = bcrypt.hashSync(pw, parseInt(process.env.SALT));
-    await User.create({userId, pw: EncryptPw, nickname});
-    res.send({msg: '회원 가입을 축하드립니다.'});
-
+    // 단방향 암호화 및 회원가입
+    await createUser(userId, pw, nickname, res);
   } catch (err) {
     printError(req, err);
     res.send({msg: err.message});
